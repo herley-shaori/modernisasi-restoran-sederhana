21642b01400975f9f853bd4eef4945db6a979604
herley <herley.shaori@gmail.com>
2025-05-04 22:30:03 +0700

    Add script to delete all images from ECR repository before destruction.

--- 1_web/infrastructure/destroy.sh
+++ 1_web/infrastructure/destroy.sh
@@ -1,1 +1,13 @@
+REPO_NAME="warungintegrasirasa-repo"
+REGION="ap-southeast-3"
+
+# Get all image digests and delete them
+IMAGE_IDS=$(aws ecr list-images --repository-name $REPO_NAME --region $REGION --query 'imageIds[*].imageDigest' --output text)
+if [ -n "$IMAGE_IDS" ]; then
+  echo "Deleting images from $REPO_NAME..."
+  aws ecr batch-delete-image --repository-name $REPO_NAME --image-ids $(echo $IMAGE_IDS | tr ' ' '\n' | sed 's/^/imageDigest=/') --region $REGION
+  echo "All images deleted."
+else
+  echo "No images found in $REPO_NAME."
+fi
 cdk destroy --all --force
